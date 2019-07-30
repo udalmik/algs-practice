39f16fe764b999f3d949fcf108772b86990af879
Mikhail Udalov <mikhail.udalov@gmail.com>
2019-07-30 21:16:41 +0300

    Swap nodes in pairs

--- js/leetcode/linked/swap-nodes-in-pairs.js
+++ js/leetcode/linked/swap-nodes-in-pairs.js
@@ -12,12 +12,12 @@ const swapPairs = head => {
         const nextLeft = left.next.next;
         const right = left.next;
         right.next = left;
-        left.next = null;
         if (!prevRight) {
             head = left.next;
         } else {
             prevRight.next = right;
         }
+        left.next = null;
         prevRight = left;
         left = nextLeft;
     }
