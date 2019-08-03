b2b43a7c469388013c4e2186eaf8ae9e7990ef9d
Mikhail Udalov <mikhail.udalov@gmail.com>
2019-08-03 22:41:39 +0300

    Is symmetric tree

--- js/leetcode/index.js
+++ js/leetcode/index.js
@@ -1,3 +1,10 @@
 
 
-const { addTwoNumbers } = require('./math/add-two-number');
+const maxArea = require('./two-pointers/water-container');
+const generateParenthesis = require('./backtracking/generate-parenthesis');
+
+const threeSum = require('./math/3-sum');
+
+
+
+console.log(threeSum([-1,0,1,2,-1]));
