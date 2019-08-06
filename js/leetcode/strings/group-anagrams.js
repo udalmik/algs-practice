2e43b62c8581758153c4cb52dd154ef24afe34f4
Mikhail Udalov <mikhail.udalov@gmail.com>
2019-08-06 22:36:18 +0300

    Group anagrams

--- js/leetcode/strings/group-anagrams.js
+++ js/leetcode/strings/group-anagrams.js
@@ -4,4 +4,14 @@
  */
 var groupAnagrams = function(strs) {
     
+    const cache = new Map();
+
+    strs.forEach(s => {
+        const key = [...s].sort().join("");
+        const arr = cache.get(key) || [];
+        arr.push(s);
+        cache.set(key, arr);
+    });
+
+    return [...cache.values()];
 };
