0f0c60f3e5e248c152a813fd4605d72081af4f67
Mikhail Udalov <mikhail.udalov@gmail.com>
2019-08-14 23:16:58 +0300

    Index of

---
+++ js/leetcode/strings/index-of.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if (needle.length > haystack.length) return -1;
+    if (needle.length === 0) return 0;
+    if (needle === haystack) return 0;
+    for (let i = 0; i < haystack.length - needle.length + 1; i++) {
+        let found = true;
+        for (let j = 0; j < needle.length; j++) {
+            if (haystack[i + j] !== needle[j]) {
+                found = false;
+                break;
+            }
+        }
+        if (found) {
+            return i;
+        }
+    }
+    return -1;
+};
