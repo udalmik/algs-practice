c5b60d3d5af5c0ee4b6222b2e0ebc9bd1bef2e30
Mikhail Udalov <mikhail.udalov@gmail.com>
2019-08-20 22:31:25 +0300

    Longest increasing subs

--- js/leetcode/dynamic/longest-increasing-sub.js
+++ js/leetcode/dynamic/longest-increasing-sub.js
@@ -19,3 +19,25 @@ var findLengthOfLCIS = function(nums) {
 
     return max;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    
+    if (nums.length <= 1) return nums.length;
+
+    const lengths = new Array(nums.length).fill(1);   
+    let max = 1;
+  
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[i] > nums[j]) {
+                lengths[i] = Math.max(lengths[j] + 1, lengths[i]);
+                max = Math.max(max, lengths[i]);
+            }
+        }
+    }
+    return max;
+};
